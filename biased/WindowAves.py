5f703c6967e028ea3022fe15acee0dce9a22a6c7
Lauren Finn <lauren.m.finn@gmail.com>
2024-03-12 16:49:55 +0100

    synced with settings selections and includes SASA analysis

--- biased/WindowAves.py
+++ biased/WindowAves.py
@@ -35,7 +35,7 @@ def main(argv):
                             dest = "state",
                             default = "holo",
                             help = ("Select state as 'apo', 'holo' or "
-                                "'mutant'."))
+                                "'apo-K57G' or 'holo-K57G'."))
         parser.add_argument("-r", "--recalc",
                             action = "store_true",
                             dest = "recalc",
@@ -48,17 +48,18 @@ def main(argv):
         print("Command line arguments are ill-defined, please check the arguments")
         raise
 
-    global path_head
+    global path_head, state
 
     recalc = args.recalc
     home = f"{ config.data_head }/{ args.path }"
     fig_path = f"{ config.figure_head }/{ args.fig_path }"
     state = args.state
+    utils.create_path(fig_path)
 
     # Get values for the restraint points
     points = pd.read_csv(f"{ home }/restraint_pts.csv")
 
-    if recalc or not os.path.exists(f"{ home }/averages.csv"): 
+    if recalc or not os.path.exists(f"{ home }/windows_averages.csv"): 
 
         df = points[["Window", "OpenPoints", "ClosedPoints"]]
 
@@ -66,20 +67,6 @@ def main(argv):
         df = df.iloc[df.index.repeat(4)]
         df = df.reset_index(drop=True)
         df['Run'] = [1, 2, 3, 4] * (len(df) // 4)
-
-        # Initialize the new columns
-        cols = ["ave_Rg", "ave_RMSD_open", "ave_RMSD_closed",
-                "K57--E200", "N53--E200", "L212--E200", "K249--K232",
-                "K249--S230a", "K249--S230b", "K249--S230c", "S230--I226",
-                "K249--E233", "R209--S231", "R209--N197", "R209--E253",
-                "R202--E210", "K221--E223", "R208--E222", "K57--G207",
-                "K57--V201", "R28--S205"]
-        if state == "holo":
-            cols.extend(["K221--IP6", "R208--IP6", "R28--IP6", 
-                "R32--IP6", "Y34--IP6", "K249--IP6", "R209--IP6", 
-                "K104--IP6", "K57--IP6", "K232--IP6", "Y234--IP6"])
-        for col in cols:
-            df[col] = 0
 
         open_state = mda.Universe(f"{ config.struct_head }/open_ref_state.pdb")
         closed_state = mda.Universe(f"{ config.struct_head }/closed_ref_state.pdb")
@@ -90,9 +77,9 @@ def main(argv):
         topol = f"{ home }/topol_protein.top"
 
         c = 0
-        nw = {"apo" : 181, "holo" : 170, "mutant" : 0}
+        nw = {"apo" : 181, "holo" : 170, "apo-K57G" : 120, "holo-K57G" : 120}
 
-        for window in np.arange(1,nw[state]):
+        for window in np.arange(1, nw[state] + 1):
             for run in [1,2,3,4]:
                 
                 run_path = f"{ home }/window{ window }/run{ run }"
@@ -116,34 +103,41 @@ def main(argv):
                 # Determine and store the RMSD to ref structures
                 ave_open = get_rmsd(u, open_state, core, ["backbone and (resid 195-218)"], "open")
                 ave_closed = get_rmsd(u, closed_state, core, ["backbone and (resid 195-218)"], "closed")
+                ave_open_alpha = get_rmsd(u, open_state, core, ["backbone and (resid 219-233)"], "open")
+                ave_closed_alpha = get_rmsd(u, closed_state, core, ["backbone and (resid 219-233)"], "closed")
                 df.loc[c, "ave_RMSD_open"] = ave_open
                 df.loc[c, "ave_RMSD_closed"] = ave_closed
+                df.loc[c, "ave_RMSD_open_alpha"] = ave_open_alpha
+                df.loc[c, "ave_RMSD_closed_alpha"] = ave_closed_alpha
 
                 # Determine the distances for critical contacts
                 ave_ds = get_contact_dists(u)
-                for col, ave_d in zip(cols[3:], ave_ds):
+                for col, ave_d in ave_ds.items():
                     df.loc[c, col] = ave_d
 
-                print(df.loc[c,:])
+                # Determine SASA for residues of interest
+                ave_sasas = get_sasas(u, run_path, f"w{ window }_r{ run }",
+                                     holo=("holo" in state))
+                for col, ave_sasa in ave_sasas.items():
+                    df.loc[c, col] = ave_sasa
 
                 c += 1
       
-        df.to_csv(f"{ home }/averages.csv")
+        df.to_csv(f"{ home }/windows_averages.csv")
 
     else: 
 
-        df = pd.read_csv(f"{ home }/averages.csv")
+        df = pd.read_csv(f"{ home }/windows_averages.csv")
 
-    print(df[8:])
-    print(df.columns[8:])
-    print("Min:", np.min(df[8:]), "\nMax:", 
-        np.max(df[8:]))
+    print(df.columns)
+    print("Min:", np.min(df), "\nMax:", 
+        np.max(df))
 
     #plot_Rgyr(df, fig_path)
     #plot_rmsd(df, "open", fig_path)
     #plot_rmsd(df, "closed", fig_path)
-    for col in df.columns[8:]:
-        plot_ave_dists(df, col, fig_path)
+    for col in df.columns:
+        plot_averages(df, col, fig_path)
     plot_windows(df, fig_path)
 
 def get_rgyr(u):
@@ -213,11 +207,7 @@ def get_rmsd(system, reference, alignment, group, ref_state):
     return rmsd_ave
 
 def get_contact_dists(u):
-    """Calculates the distance for a key salt bridge.
-
-    Calculates the distance between the nitrogen in the side chain of the 
-    lysine residue (K57) or the alpha carbon for the mutated residue (K57G) and
-    the oxygen in the side chain of glutamate (E200).
+    """Calculates the distance for critical contacts.
 
     Parameters
     ----------
@@ -226,72 +216,113 @@ def get_contact_dists(u):
     
     Returns
     -------
-    ave_d : float
-        The mean distance K57--E200 in Angstrom. 
+    ave_d : (float) dict
+        The mean distance for tabulated critical contacts.
     """
-    acid_base_pairs = [
-            (("resid 57 and name NZ*"),("resid 200 and name OE*")),
-            (("resid 53 and name ND2"),("resid 200 and name OE*")),
-            (("resid 212 and name N"),("resid 200 and name O")),
-            (("resid 249 and name NZ"),("resid 232 and name O")),
-            (("resid 249 and name N"),("resid 230 and name OG")),
-            (("resid 249 and name N"),("resid 230 and name O")),
-            (("resid 249 and name NZ"),("resid 230 and name O")),
-            (("resid 230 and name N"),("resid 226 and name O")),
-            (("resid 249 and name NZ"),("resid 233 and name OE*")),
-            (("resid 209 and name NH*"),("resid 231 and (name OG or name O)")),
-            (("resid 209 and name NH*"),("resid 197 and name OD*")),
-            (("resid 209 and name NH*"),("resid 253 and name OE*")),
-            (("resid 202 and name NE"),("resid 210 and name OE*")),
-            (("resid 221 and name NZ"),("resid 223 and name OE*")),
-            (("resid 208 and name NH*"),("resid 222 and name OE*")),
-            (("resid 57 and name NZ"),("resid 207 and name O")),
-            (("resid 57 and name NZ"),("resid 201 and name O")),
-            (("resid 28 and name NH*"),("resid 205 and name O")),
-            # (("resid 221 and name NZ"),("resname IPL and name O*")),
-            # (("resid 208 and name N*"),("resname IPL and name O*")),
-            # (("resid 28 and name NH*"),("resname IPL and name O*")),
-            # (("resid 32 and name NH*"),("resname IPL and name O*")),
-            # (("resid 34 and name OH"),("resname IPL and name O*")),
-            # (("resid 249 and name NZ"),("resname IPL and name O*")),
-            # (("resid 209 and name NH*"),("resname IPL and name O*")),
-            # (("resid 104 and name NZ"),("resname IPL and name O*")),
-            # (("resid 57 and name NZ"),("resname IPL and name O*")),
-            # (("resid 232 and name NZ"),("resname IPL and name O*")),
-            # (("resid 234 and name OH"),("resname IPL and name O*")),
-    ]
 
-    pairs = []
+    # A list of tuples for selection strings of the contacts of interest
+    from config.settings import selections
 
-    for b, a in acid_base_pairs:
-        sel_basic = u.select_atoms(b)
-        sel_acidic = u.select_atoms(a)
-        pairs.append((sel_basic, sel_acidic))
+    averages = {}
 
-    print(pairs)
+    for key, select in selections.items():
 
-    #dist_pair1 = distance_array(u.coord[ca_pair1_1], u.coord[ca_pair1_2])
-    distances = np.zeros((u.trajectory.n_frames, len(acid_base_pairs)))
+        if ("IP6" in key) & ("holo" not in state):
+            continue
 
-    # Loop over all frames in the trajectory
-    for ts in u.trajectory:
-        # Calculate the distances between the four acid-base pairs for this frame
-        for i in range(len(pairs)):
-            d = distance_array(pairs[i][0].positions, pairs[i][1].positions)
+        # Define the distance using the tuple of selection strings
+        sel_a = u.select_atoms(select[0])
+        sel_b = u.select_atoms(select[1])
 
-            # Store the distances in the distances array
-            distances[ts.frame, i] = np.min(d)
+        # Iterate over trajectory framse to get the time series
+        distances = np.zeros(u.trajectory.n_frames)
+        for ts in u.trajectory:
+            d = distance_array(sel_a.positions, sel_b.positions)
+            # Use the smallest pair distance
+            distances[ts.frame] = np.min(d)
 
-    ave_d = np.mean(distances, axis=0)
-    print(f"Average distances for window: \n{ ave_d }\nShape { ave_d.shape }")
+        ave_d = np.mean(distances, axis=0)
+        averages[key] = ave_d
 
-    return ave_d
+    return averages
+
+def get_sasas(u, path, sim_name, holo=False):
+    """Calculates the SASA for residues.
+
+    Parameters
+    ----------
+    system : MDAnalysis.core.universe
+        The universe object, with the trajectory loaded in.
+    path : str
+        Path to the individual window data
+    sim_name : str
+        Name used for the .tpr file for the window.
+    
+    Returns
+    -------
+    ave_sasas : (float) dict
+        The mean SASA for tabulated residues.
+
+    """
+    import subprocess
+    # A dictionary for the residues this interesting SASA data
+    resids_water = {"TRP 218 SASA" : 218, "LYS 57 SASA" : 57, 
+        "GLU 200 SASA" : 200}
+
+    ave_sasas = {}
+
+    for key, resid in resids_water.items():
+
+        # Use a subprocess to get SASA for a residue using the gromacs tool
+        sasa_path = f"{ path }/sasa_{ resid }.xvg"
+
+        # Use gromacs subprocess to get SASA data
+        if not os.path.exists(sasa_path):
+
+            # Setup variables for command-line arguements
+            p = path
+            if holo:
+                surface = 24 
+                output = 25
+            else:
+                surface = 1
+                output = 19
+
+            # command-line arguements
+            gmx_ndx = (f"""echo "ri { resid }\nq" | gmx22 make_ndx -f """
+                f"""{ p }/{ sim_name }.tpr -o { p }/index_{ resid }.ndx """
+                """-nobackup""")
+            gmx_sasa = (f"gmx22 sasa -f { p }/fitted_traj.xtc -s " 
+                f"{ p }/{ sim_name }.tpr -o { p }/sasa_{ resid }.xvg"
+                f" -or { p }/res{ resid }_sasa.xvg -surface { surface } "
+                f"-output { output } -n { p }/index_{ resid }.ndx "
+                f"-nobackup")
+
+            # Calculate SASA with gmx sasa using a subprocess
+            if holo:
+                gmx_ndx.extend(f" -n { p }/index.ndx")
+            subprocess.run(gmx_ndx, shell=True)
+            subprocess.run(gmx_sasa, shell=True)
+
+        # In case something went wrong in the subprocess...
+        utils.validate_path(sasa_path, warning="Use 'gmx sasa' to extract "
+            "sasa data.\n")
+
+        # Read in the gromacs analysis output
+        gmx_sasa = np.loadtxt(sasa_path, comments=["#", "@"])
+        sasa_data = gmx_sasa[:,2]
+
+        # Get the window average into a dictionary
+        ave_sasa = np.mean(sasa_data, axis=0)
+        print(f"Average { key } SASA for { sim_name }: \n\t{ ave_sasa }")
+        ave_sasas[key] = ave_sasa
+
+    return ave_sasas
 
 def plot_Rgyr(df, path):
     fig, ax = plt.subplots(constrained_layout=True, figsize=(12,8))
 
     ave_Rg = df.groupby('Id')['ave_Rg'].min().values
-    print(ave_Rg)
 
     d = ax.scatter(df[df["Run"] == 1]["OpenPoint"], 
                     df[df["Run"] == 1]["ClosedPoint"], c=ave_Rg, 
@@ -326,30 +357,40 @@ def plot_rmsd(df, ref_state, path):
     plt.savefig(f"{ path }/window_RMSD_{ ref_state }.png", dpi=300)
     plt.close()
 
-def plot_ave_dists(df, col, path):
-    fig, ax = plt.subplots(constrained_layout=True, figsize=(12,8))
+def plot_averages(df, col, path):
+    fig, ax = plt.subplots(constrained_layout=True, figsize=(10,8))
 
-    ave_salt = df.groupby('Window')[col].mean().values
-    print(min(ave_salt), max(ave_salt))
+    ave_val = df.groupby('Window')[col].mean().values
+    print(col, min(ave_val), max(ave_val))
 
-    d = ax.scatter(df[df["Run"] == 1]["OpenPoints"], 
-                    df[df["Run"] == 1]["ClosedPoints"], c=ave_salt, 
-                    cmap="coolwarm", marker="o", edgecolors="#404040", 
-                    s=200, lw=2, norm=mcolors.Normalize(vmin=2, vmax=27))
+    if "SASA" in col:
+        d = ax.tricontourf(df[df["Run"] == 1]["OpenPoints"], 
+                    df[df["Run"] == 1]["ClosedPoints"], ave_salt, 10,
+                    cmap="coolwarm",  vmin=1, vmax=25)
+        # Colormap settings
+        cbar = plt.colorbar(d)
+        cbar.set_label(f"{ col } " + r"(nm$^2$)", fontsize=28, 
+            labelpad=10)
 
-    # Colormap settings
-    cbar = plt.colorbar(d, ticks=np.arange(2, 27.1, 5))
-    cbar.set_label(f"Distance { col } " + r"($\AA$)", fontsize=32, 
-        labelpad=10)
+    else: 
+        d = ax.tricontourf(df[df["Run"] == 1]["OpenPoints"], 
+                    df[df["Run"] == 1]["ClosedPoints"], ave_salt, 10,
+                    cmap="coolwarm")
 
-    ax.set_xlabel(r"$\xi_1$ (nm$^2$)")
-    ax.set_ylabel(r"$\xi_2$ (nm$^2$)")
+        # Colormap settings
+        cbar = plt.colorbar(d, ticks=np.arange(1, 25.1, 4), extend="both")
+        cbar.set_label(f"Distance { col } " + r"($\AA$)", fontsize=28, 
+            labelpad=10)
+
+    # Add labels etc. 
+    ax.set_xlabel(r"$\xi_1$ (nm$^2$)", fontsize=28)
+    ax.set_ylabel(r"$\xi_2$ (nm$^2$)", fontsize=28)
 
     plt.savefig(f"{ path }/window_ave_{ col }.png", dpi=300)
     plt.close()
 
 def plot_windows(df, path):
-    fig, ax = plt.subplots(constrained_layout=True, figsize=(12,8))
+    fig, ax = plt.subplots(constrained_layout=True, figsize=(8,8))
 
     window_labs = df.groupby('Window')["Window"].mean().values.astype(int)
     x = np.array(df[df["Run"] == 1]["OpenPoints"])
